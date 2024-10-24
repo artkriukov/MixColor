ccac14f6ab87901887d31be647bfa6cbbefd5294
Artem Kriukov <art.kriukov@gmail.com>
2024-10-24 19:13:23 +0300

    worked with logic

--- MixColor/ViewController.swift
+++ MixColor/ViewController.swift
@@ -7,13 +7,45 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
+    
+    @IBOutlet weak var colorView: UIView!
+    
+    @IBOutlet weak var redSlider: UISlider!
+    @IBOutlet weak var greenSlider: UISlider!
+    @IBOutlet weak var blueSlider: UISlider!
+    
+    
 
+    @IBOutlet weak var redValueLabel: UILabel!
+    @IBOutlet weak var greenValueLabel: UILabel!
+    @IBOutlet weak var blueValueLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        colorView.layer.cornerRadius = 20
     }
-
-
+    
+    
+    
+    
+    @IBAction func sliderValueChanged(_ sender: UISlider) {
+        let color = UIColor(
+            red: CGFloat(redSlider.value),
+            green: CGFloat(greenSlider.value),
+            blue: CGFloat(blueSlider.value),
+            alpha: 1
+        )
+        colorView.backgroundColor = color
+        
+        updateLabels()
+    }
+    
+    func updateLabels() {
+        redValueLabel.text = String(format: "Red: %.2f", redSlider.value)
+        greenValueLabel.text = String(format: "Green: %.2f", greenSlider.value)
+        blueValueLabel.text = String(format: "Blue: %.2f", blueSlider.value)
+    }
 }
 
