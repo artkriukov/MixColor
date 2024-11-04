aac9eeea0c7acb91608505df05d8d80396c70b68
Artem Kriukov <art.kriukov@gmail.com>
2024-11-04 18:23:18 +0300

    feat: implement RGB color control functionality with sliders and color preview
    - Added `colorView` to display the selected color with rounded corners.
    - Added `redValueLabel`, `greenValueLabel`, and `blueValueLabel` to show the RGB values.
    - Configured `redSliderValue`, `greenSliderValue`, and `blueSliderValue` to adjust RGB values.
    - Implemented `sliderValueChanged` method to update the `colorView` background color based on slider values.
    - Created `updateLabels` method to display RGB values in labels with two decimal precision.

--- MixColor/ViewController.swift
+++ MixColor/ViewController.swift
@@ -7,13 +7,45 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
 
+    @IBOutlet weak var colorView: UIView!
+    
+    @IBOutlet weak var redValueLabel: UILabel!
+    @IBOutlet weak var greenValueLabel: UILabel!
+    @IBOutlet weak var blueValueLabel: UILabel!
+    
+    @IBOutlet weak var redSliderValue: UISlider!
+    @IBOutlet weak var greenSliderValue: UISlider!
+    @IBOutlet weak var blueSliderValue: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        colorView.layer.cornerRadius = 10
+        updateLabels()
     }
-
-
+    
+    @IBAction func sliderValueChanged(_ sender: UISlider) {
+        let color = UIColor(
+            red: CGFloat(redSliderValue.value),
+            green: CGFloat(greenSliderValue.value),
+            blue: CGFloat(blueSliderValue.value),
+            alpha: 1
+        )
+        
+        colorView.backgroundColor = color
+        updateLabels()
+    }
+    
+    func updateLabels() {
+        redValueLabel.text = String()
+        greenValueLabel.text = String()
+        blueValueLabel.text = String()
+        
+        redValueLabel.text = String(format: "%.2f", redSliderValue.value)
+        greenValueLabel.text = String(format: "%.2f", greenSliderValue.value)
+        blueValueLabel.text = String(format: "%.2f", blueSliderValue.value)
+    }
 }
 
